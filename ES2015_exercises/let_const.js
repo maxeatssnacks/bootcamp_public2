704515015bb594048b2272545c3209290776c8a9
Max Rautenkranz <maxrautenkranz@Maxs-MacBook-Pro.local>
2023-10-23 20:10:27 -0500

    add ES2015

---
+++ ES2015_exercises/let_const.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+PI = 42;
+
+// Var is bound to a function, let is bound to a code block. Var can be reassigned and redeclared, Let cannot be redeclared.
+    // You can access a hoisted variable with var.
+// Var is bound to a function and can be reassigned or redeclared, const is bound to a code block and cannot be redeclared or reassigned.
+    // You can access a hoisted variable with var.
+// Let can be reassigned, const can not.
+// Hoisting is what happens when you use VAR to initialize your variables. It runs them first before all of the other code is ran, which
+    // can lead to some weird interactions (undefined vs non-initialized errors)
