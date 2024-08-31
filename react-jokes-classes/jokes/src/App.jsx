3baa5696bb608e061c9f92e79af8dc8a52345d74
Max Rautenkranz <maxrautenkranz@Maxs-MacBook-Pro.local>
2024-08-31 12:29:59 -0500

    CheeZJokes App Refactored

---
+++ react-jokes-classes/jokes/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import JokeList from "./JokeList";
+
+/** App component. Renders list of jokes. */
+
+const App = () => {
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  )
+}
+
+export default App;
---
+++ react-jokes-classes/jokes/src/JokeList.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Joke from "./Joke";
+import "./JokeList.css";
+
+const JokeList = ({ numJokesToGet = 5 }) => {
+  const [jokes, setJokes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getJokes = async () => {
+    try {
+      let newJokes = [];
+      let seenJokes = new Set();
+
+      while (newJokes.length < numJokesToGet) {
+        let res = await axios.get("https://icanhazdadjoke.com", {
+          headers: { Accept: "application/json" }
+        });
+        let { ...joke } = res.data;
+
+        if (!seenJokes.has(joke.id)) {
+          seenJokes.add(joke.id);
+          newJokes.push({ ...joke, votes: 0 });
+        } else {
+          console.log("duplicate found!");
+        }
+      }
+
+      setJokes(newJokes);
+      setIsLoading(false);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getJokes();
+  }, []);
+
+  const generateNewJokes = () => {
+    setIsLoading(true);
+    getJokes();
+  };
+
+  const vote = (id, delta) => {
+    setJokes(prevJokes =>
+      prevJokes.map(j =>
+        j.id === id ? { ...j, votes: j.votes + delta } : j
+      )
+    );
+  };
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <i className="fas fa-4x fa-spinner fa-spin" />
+      </div>
+    );
+  }
+
+  let sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
+
+  return (
+    <div className="JokeList">
+      <button
+        className="JokeList-getmore"
+        onClick={generateNewJokes}
+      >
+        Get New Jokes
+      </button>
+
+      {sortedJokes.map(j => (
+        <Joke
+          text={j.joke}
+          key={j.id}
+          id={j.id}
+          votes={j.votes}
+          vote={vote}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default JokeList;
